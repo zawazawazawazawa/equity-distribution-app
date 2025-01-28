d87a3f140cafc167f02938a66cab7172e93abc56
zawazawazawazawa <bosse.de.poulet@gmail.com>
2025-01-29 00:20:11 +0900

    Initial commit by cline

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module equity-distribution-backend
+
+go 1.13
---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Equity Distribution Backend is running")
+	})
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
