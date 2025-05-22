22457055133c030571e1eda1c9c71f42c0901fb2
zawazawazawazawa <bosse.de.poulet@gmail.com>
2025-05-22 23:55:57 +0900

    Remove unused files

--- types/poker.ts
+++
@@ -1,7 +0,0 @@
-export type GameMode = 'hand-vs-hand' | 'hand-vs-range';
-
-export interface GameModeState {
-  mode: GameMode;
-  heroHand: string;
-  villainInput: string | string[]; // handの場合はstring、rangeの場合はstring[]
-}
